b1f29af25a46d94177f413ffafebb734b1b1343b
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-09-28 11:07:26 +0000

    a

--- Ex_Aula_2.py
+++ Ex_Aula_2.py
@@ -14,8 +14,13 @@ def comfor(num1, num2):
 
 def comwhile(num1, num2):
     total = 0
-    while x in range(num1, num2):
-        return x
+    while num1 < num2:
+        total += num1
+        num1 += 1
+        if num1 == num2:
+            break
+
+    return total
 
 
 if __name__ == '__main__':
@@ -31,6 +36,13 @@ if __name__ == '__main__':
 
             if opcao == 1:
                 print(f'Total = {comfor(num1, num2)}')
+            if opcao == 2:
+                num2 += 1
+                print(f'Total = {comwhile(num1, num2)}')
+
+            continuar = input('Deseja continuar (n or s?')
+            if continuar == 'n':
+                break
 
         except ValueError:
             print('Digite um valor inteiro!')
