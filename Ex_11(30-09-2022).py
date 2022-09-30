6c9d435210a6cd4aacc6a736025a35281e9931b1
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-09-30 10:18:35 +0000

    30-09-2022 10:18 funcionando num! = num * (num - 1) aula

---
+++ Ex_11(30-09-2022).py
@@ -0,0 +1,10 @@
+def factorial(num):
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+if __name__ == '__main__':
+    num = int(input('Digite um numeriro: '))
+
+    print(factorial(num))
