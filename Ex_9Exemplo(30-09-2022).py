b6f2d1c0158a70100ccf0e7f08567b4ca70c849c
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-09-30 10:10:10 +0000

    30-09-2022 08:54 funcionando aula

--- Ex_9Exemplo(30-09-2022).py
+++ Ex_9Exemplo(30-09-2022).py
@@ -1,20 +1,21 @@
-#pessoa = {1, 'Maria', {'morada': 'Rua de Cima, 1', 'cp': 9533}, 12.56, [123, 434, 3434, 334.3]}
+pessoa = (1, 'Maria', {'morada': 'Rua de Cima, 1', 'cp': 9500}, 12.50, [123, 434, 3434, 334.3])
 
 meses = {'Janeiro', 'Fevereiro', 'Março'}
 
-
 if __name__ == '__main__':
-    # Não funcionando
-    """print(pessoa[2].keys())
+    """
+    print(pessoa[2].keys())
     print(pessoa[2].values())
     print()
-    for x in pessoa[2].keys():
-        print(x)
+    for k in pessoa[2].keys():
+        print(k)
+    print()
     for v in pessoa[2].values():
         print(v)
     print()
     for k in pessoa[2].keys():
-        print(f'{k} = {pessoa[2][k]}')"""
+        print(f'{k} = {pessoa[2][k]}')
+    """
 
     meses.add('Abril')
     print(meses)
